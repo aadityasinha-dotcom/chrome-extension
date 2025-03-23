a25380da6fb9ff325ffa7ed47a4e03ff6166cbd6
aadityasinha-dotcom <aadityasinha009@gmail.com>
2025-03-23 23:41:39 +0530

    removed logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import reactLogo from './assets/react.svg'
 import './App.css'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
@@ -15,9 +15,9 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* <a href="https://react.dev" target="_blank"> */}
+        {/*   <img src={reactLogo} className="logo react" alt="React logo" /> */}
+        {/* </a> */}
       </div>
       {isPageLoading}
       <div className="models">
